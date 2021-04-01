1ce70d1a2aed8c39a56d795061fb41e229ae7ef9
Evgeniy Finogeev <finogeev@ap-team.ru>
2021-04-01 18:14:25 +0300

    create autefication

--- WEB_clustering/f_clustering/views.py
+++ WEB_clustering/f_clustering/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from .forms import AuthUserForm, RegisterUserForm
 
+from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView, LogoutView
 # Create your views here.
+
+def main(request):
+	context = {
+		'name': 'Pidr'
+	}
+	template = 'main.html'
+	return render(request, template, context)
+
+class SpeechLoginView(LoginView):
+	template_name = 'login.html'
+	form_class = AuthUserForm
+	success_url = reverse_lazy('main')
+	def get_success_url(self):
+		return self.success_url
+
+class SpeechRegisterView(CreateView):
+	model = User
+	template_name = 'register.html'
+	form_class = RegisterUserForm
+	success_url = reverse_lazy('main')
+	def form_valid(self, form):
+		form_valid = super().form_valid(form)
+		username = form.cleaned_data["username"]
+		password = form.cleaned_data["password"]
+		auth_user = authenticate(username=username, password=password)
+		login(self.request, auth_user)
+		return form_valid
+		
+class SpeechLogout(LogoutView):
+	next_page = reverse_lazy('main')
